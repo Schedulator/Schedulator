6c906bca5a25873d435fce998e2514627131128d
harley1011 <harley.1011@gmail.com>
2015-02-26 21:33:14 -0700

    Removed some unnecessary files

--- Schedulator/Controllers/HomeController.cs
+++ Schedulator/Controllers/HomeController.cs
@@ -16,20 +16,5 @@ namespace Schedulator.Controllers
         {
             return View(db.Users.Find(User.Identity.GetUserId<int>()));
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
     }
 }
