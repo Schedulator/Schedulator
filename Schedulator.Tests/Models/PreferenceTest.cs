8213c72199b2408745ccea825b219386b66472e8
unknown <austin.hubbell@gmail.com>
2015-03-19 23:31:10 -0700

    Started unit testing for models

---
+++ Schedulator.Tests/Models/PreferenceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Schedulator.Tests.Models
+{
+    [TestClass]
+    public class PreferenceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
