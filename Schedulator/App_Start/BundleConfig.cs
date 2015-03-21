3c0a088bf3aecd8c71364bce86263afc7ed34e85
Sam Kazerouni <samkazerouni@gmail.com>
2015-03-21 18:53:29 -0400

    Brand new layout, brand new logo. more responsive site

--- Schedulator/App_Start/BundleConfig.cs
+++ Schedulator/App_Start/BundleConfig.cs
@@ -9,6 +9,9 @@ namespace Schedulator
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jQuery.prettyPhoto.js",
+                        "~/Scripts/jQuery.js",
+                        "~/Scripts/jQuery.isotope.min.js",
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -20,12 +23,22 @@ namespace Schedulator
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/wow.min.js",
+                      "~/Scripts/respond.min.js",
+                      "~/Scripts/main.js",
+                      "~/Scripts/html5shiv.js",
+                      "~/Scripts/boostrap.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/animate.min.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/main.css",
+                      "~/Content/prettyPhoto.css",
+                      "~/Content/responsive.css",
+                      "~/Content/Site.css"));
         }
     }
 }
