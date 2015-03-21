73924e25d91fae1f55d6ca6744c3aff21ac2e971
lucson312 <lucson312@gmail.com>
2015-03-21 00:34:25 -0400

    Added new general css file.

--- Schedulator/App_Start/BundleConfig.cs
+++ Schedulator/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Schedulator
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", "~/Content/Schedulator.css"));
         }
     }
 }
