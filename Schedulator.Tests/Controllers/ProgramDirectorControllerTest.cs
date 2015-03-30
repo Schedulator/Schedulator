db450bc34ae9373ae8c2c6012bfbe5d1a7aefb6c
unknown <austin.hubbell@gmail.com>
2015-03-29 22:04:32 -0700

    Added testing files for models, added new testing files for controllers. Started unit testing of models.

---
+++ Schedulator.Tests/Controllers/ProgramDirectorControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Schedulator.Tests.Controllers
+{
+    [TestClass]
+    public class ProgramDirectorControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
