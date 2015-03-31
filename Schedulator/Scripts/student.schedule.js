436291acee0bd4d4076659189c37d342ce4c21aa
harley1011 <harley.1011@gmail.com>
2015-03-30 22:03:34 -0600

    Worked on remove course from schedule feature on schedule tab

--- Schedulator/Scripts/student.schedule.js
+++ Schedulator/Scripts/student.schedule.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-
     $(".schedule-semester").click(function () {
         var url = '/StudentSchedule/GetSchedule?semester=' + $(this).attr('semester');
         $("#schedule-div").load(url);
-    })
+    });
+
+
 });
