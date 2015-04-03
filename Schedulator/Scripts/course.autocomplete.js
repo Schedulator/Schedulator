20f44f8db83b7be616bfbd97f68b7efedc3f47ba
harley1011 <harley.1011@gmail.com>
2015-04-03 15:16:13 -0600

    Added some client side validation for add course text box

--- Schedulator/Scripts/course.autocomplete.js
+++ Schedulator/Scripts/course.autocomplete.js
@@ -13,30 +13,41 @@
 		}
 	});
 }
+function checkIfCourseExist(courseName)
+{
+	var courseExist = $.grep(courseList, function (e) { return e.label == courseName; }).length == 1;
+	var courseAlreadyAdded = $.inArray(courseName, addedCourseList) == -1
+	if (courseExist && courseAlreadyAdded)
+		return true;
+	else if (courseExist)
+		alert("Course already added");
+	else
+		alert("Course doesn't exist");
+	return false;
+
+}
 var count = 0;
 var courseList;
-var url = '/ScheduleGenerator/CoursesViewJson';
-
+var addedCourseList = [];
 $(document).ready(function () {
 	$("#addCourse").click(function () {
 		var name = $("input[name='courseName']").val();
 		$("input[name='courseName']").val("");
-		$("#selectedCourses").append("<div class='col-sm-1 courseBlock'>" + name + "<input name='courseCode[" + count + "]' value='" + name + "' hidden></div>");
-		count++;
+		if (checkIfCourseExist(name)) {
+			$("#selectedCourses").append("<div class='col-sm-1 courseBlock'>" + name + "<input name='courseCode[" + count + "]' value='" + name + "' hidden></div>");
+			count++;
+			addedCourseList.push(name);
+		}		
 	});
 
 	$(".removeClass").click(function () {
 		this.parent().hide();
 	});
 
-	$.getJSON(url, function (data) {
+	$.getJSON('/ScheduleGenerator/CoursesViewJson', function (data) {
 		courseList = data;
 	});
 
-	$("#amwork").click(function(e) {  
-		alert(1);
-
-	});
 	$(document).on("click", ".courseBlock", function () {
 		$(this).closest("div").remove();
 	});
