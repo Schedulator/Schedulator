860c4df07e7d9b07ccc2366789caa4144f553e70
Harrison Ianatchkov <harrison1993@gmail.com>
2015-04-02 23:29:26 -0400

    Added the getGPA method to enrollment class

--- Schedulator/Models/Enrollment.cs
+++ Schedulator/Models/Enrollment.cs
@@ -15,7 +15,25 @@ namespace Schedulator.Models
         public virtual Course Course { get; set; }
         public virtual Section Section { get; set; }
 
-
+        public double getGPA()
+        {
+            switch (Grade)
+            {
+                case "A+" : return 4.3;
+                case "A"  : return 4.0;
+                case "A-" : return 3.7;
+                case "B+" : return 3.3;
+                case "B"  : return 3.0;
+                case "B-" : return 2.7;
+                case "C+" : return 2.3;
+                case "C"  : return 2.0;
+                case "C-" : return 1.7;
+                case "D+" : return 1.3;
+                case "D"  : return 1.0;
+                case "D-" : return 0.7;
+                default   : return 0.0;
+            }
+        }
     }
 
    
