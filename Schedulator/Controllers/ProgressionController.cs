ed8cce2249e3dcc1d1e63354a129d475d4c7bedc
Harrison Ianatchkov <harrison1993@gmail.com>
2015-04-02 21:56:55 -0400

    Removed some garbage code from the controller

--- Schedulator/Controllers/ProgressionController.cs
+++ Schedulator/Controllers/ProgressionController.cs
@@ -11,19 +11,15 @@ namespace Schedulator.Controllers
     [Authorize]
     public class ProgressionController : Controller
     {
-        // GET: Progression
         ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             string userId = User.Identity.GetUserId();
 
-            Course course = db.Courses.Where(n => n.CourseLetters == "COMP" && n.CourseNumber == 345).FirstOrDefault();
             List<Enrollment> studentEnrollments = db.Enrollment.Where(n => n.Schedule.ApplicationUser.Id == userId).ToList();
             Program program = db.Users.Find(User.Identity.GetUserId()).Program;
             List<CourseSequence> courseSequences = db.CourseSequence.Where(n => n.Program.ProgramId == program.ProgramId && n.ContainerSequence == null).ToList();
-
-            List<CourseSequence> courseSequences2 = db.CourseSequence.Where(n => n.Program.ProgramId == program.ProgramId ).ToList();
-            Progression studentsProgression = new Progression() { ProgessionUnitList = new List<Progression.ProgressionUnit>()};
+            Progression studentsProgression = new Progression() { ProgessionUnitList = new List<Progression.ProgressionUnit>() };
             studentsProgression.StudentsProgression(studentEnrollments, courseSequences);
 
             return View(studentsProgression);
