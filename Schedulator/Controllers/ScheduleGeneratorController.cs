e4ec576e6725c2e29c05e7a496398a88349fa95e
harley1011 <harley.1011@gmail.com>
2015-04-04 16:07:02 -0600

    Added view

--- Schedulator/Controllers/ScheduleGeneratorController.cs
+++ Schedulator/Controllers/ScheduleGeneratorController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using System.Diagnostics;
 
 namespace Schedulator.Controllers
 {
@@ -135,7 +136,83 @@ namespace Schedulator.Controllers
             }            
             ScheduleGenerator scheduleGenerator = new ScheduleGenerator { Preference = preference };
             string user = db.Users.Find(User.Identity.GetUserId()).Email;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
             scheduleGenerator.GenerateSchedules(db.Courses.ToList(), db.Enrollment.Where(n => n.Schedule.ApplicationUser.Email == user).ToList());
+            stopWatch.Stop();
+            long duration = stopWatch.ElapsedMilliseconds;
+            try
+            {
+                if (scheduleGenerator.Schedules.Count() > 40)
+                    scheduleGenerator.Schedules = scheduleGenerator.Schedules.GetRange(0, 40);
+            }
+            catch { }
+            
+            return PartialView("_GenScheduleResultPartial", scheduleGenerator);
+        }
+        [HttpPost]
+        public ActionResult GenerateSchedulesPaging(List<String> courseCode, String semester, List<String> timeOption, int pageNumber)
+        {
+
+            Season season;
+            switch (semester)
+            {
+                case "Summer": season = Season.Summer1;
+                    break;
+                case "Fall": season = Season.Fall;
+                    break;
+                case "Winter": season = Season.Winter;
+                    break;
+                default: season = Season.Fall;
+                    break;
+            }
+            double startTime = 0;
+            double endTime = 0;
+            if (timeOption != null)
+            {
+                foreach (string timeType in timeOption)
+                {
+                    if (timeType == "Morning")
+                    {
+                        startTime = 1;
+                        endTime = 720;
+                    }
+                    else if (timeType == "Day")
+                    {
+                        startTime = (startTime == 1) ? startTime : 720;
+                        endTime = (endTime < 1080) ? 1080 : endTime;
+                    }
+                    else if (timeType == "Night")
+                    {
+                        startTime = (startTime == 1 || startTime == 720) ? startTime : 1080;
+                        endTime = (endTime < 1440) ? 1440 : endTime;
+                    }
+                }
+            }
+            endTime = (endTime == 0) ? 1440 : endTime;
+            Preference preference = new Preference { Semester = db.Semesters.Where(n => n.Season == season).FirstOrDefault(), StartTime = startTime, EndTime = endTime };
+            preference.Courses = new List<Course>();
+
+            foreach (var code in courseCode)
+            {
+                String[] courseID = code.Split(' ');
+                var courseLetter = courseID[0];
+                int courseNumber = Int32.Parse(courseID[1]);
+                preference.Courses.Add(db.Courses.Where(n => n.CourseNumber == courseNumber && n.CourseLetters == courseLetter).FirstOrDefault());
+            }
+            ScheduleGenerator scheduleGenerator = new ScheduleGenerator { Preference = preference };
+            string user = db.Users.Find(User.Identity.GetUserId()).Email;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            scheduleGenerator.GenerateSchedules(db.Courses.ToList(), db.Enrollment.Where(n => n.Schedule.ApplicationUser.Email == user).ToList());
+            stopWatch.Stop();
+            long duration = stopWatch.ElapsedMilliseconds;
+            try
+            {
+                if (scheduleGenerator.Schedules.Count() > 40)
+                    scheduleGenerator.Schedules = scheduleGenerator.Schedules.GetRange(pageNumber, pageNumber + 40);
+            }
+            catch { }
 
             return PartialView("_GenScheduleResultPartial", scheduleGenerator);
         }
