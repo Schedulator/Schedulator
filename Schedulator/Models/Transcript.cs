72a67217216b265736fbe413f6ed2ed3d4fa4614
Harrison Ianatchkov <harrison1993@gmail.com>
2015-04-03 22:59:59 -0400

    Ca s'en vien la. It's all commented so it won't affect the solution. Just trying some things. This is just so you can see where I'm going with it.

--- Schedulator/Models/Transcript.cs
+++ Schedulator/Models/Transcript.cs
@@ -10,74 +10,77 @@ namespace Schedulator.Models
     public class Transcript
     {
 
-        public List<TranscriptYear> StudentTranscriptYear { get; set; }
+        //public List<TranscriptYear> TrasncriptYears { get; set; }
 
-        public class TranscriptYear
-        {
-            public int Year { get; set; }
-            public List<TranscriptSemester> StudentYearEnrollments { get; set; }
+        //public class TranscriptYear
+        //{
+        //    public int Year { get; set; }
+        //    public List<TranscriptSemester> TranscriptSemesters { get; set; }
 
-            public class TranscriptSemester
-            {
-                public Semester Semester { get; set; }
-                public List<Enrollment> StudentEnrollments { get; set; }
-            }
-        }
+        //    public class TranscriptSemester
+        //    {
+        //        public Semester Semester { get; set; }
+        //        public List<Enrollment> Enrollments { get; set; }
+        //    }
+        //}
 
 
-        public void StudentTranscript(List<Enrollment> studentEnrollments)
-        {
-            ApplicationDbContext db = new ApplicationDbContext();
-            if (studentEnrollments != null && studentEnrollments.Count != 0) 
-            {
-                int studentStartYear = studentEnrollments.FirstOrDefault().Schedule.Semester.SemesterStart.Year;
-                Season startSeason = studentEnrollments.FirstOrDefault().Schedule.Semester.Season;
+        //public void StudentTranscript(List<Enrollment> studentEnrollments)
+        //{
+        //    ApplicationDbContext db = new ApplicationDbContext();
+        //    if (studentEnrollments != null && studentEnrollments.Count != 0) 
+        //    {
+        //        int studentStartYear = studentEnrollments.FirstOrDefault().Schedule.Semester.SemesterStart.Year;
+        //        Season startSeason = studentEnrollments.FirstOrDefault().Schedule.Semester.Season;
                 
 
-                HashSet<int> studentEnrollmentYearSpan = new HashSet<int>();
-                foreach (Enrollment enrollment in studentEnrollments)
-                {
-                    studentEnrollmentYearSpan.Add(enrollment.Schedule.Semester.SemesterStart.Year);
-                }
-                foreach (int year in studentEnrollmentYearSpan)
-                {
-                    StudentTranscriptYear.Add(new TranscriptYear { Year = year });
+        //        HashSet<int> studentEnrollmentYearSpan = new HashSet<int>();
+        //        foreach (Enrollment enrollment in studentEnrollments)
+        //            studentEnrollmentYearSpan.Add(enrollment.Schedule.Semester.SemesterStart.Year);
+               
+        //        foreach (int year in studentEnrollmentYearSpan)
+        //        {
+        //            TrasncriptYears.Add(new TranscriptYear { Year = year });
+        //            foreach (Enrollment enrollment in studentEnrollments.Where(n => n.Schedule.Semester.SemesterStart.Year == year))
+        //            {
+        //                for 
+        //            }
 
 
-                    //switch (startSeason)
-                    //{
-                    //    case Season.Fall:
-                    //        {
-                    //            createTranscript(startSeason, studentStartYear);
-                    //        }
-                    //        break;
-                    //    case Season.Winter:
-                    //        {
+        //            //switch (startSeason)
+        //            //{
+        //            //    case Season.Fall:
+        //            //        {
+        //            //            createTranscript(startSeason, studentStartYear);
+        //            //        }
+        //            //        break;
+        //            //    case Season.Winter:
+        //            //        {
 
-                    //        }
-                    //        break;
-                    //    case Season.Summer1:
-                    //        {
-                    //            throw new NullReferenceException("Trying to start from a summer semester");
-                    //        }
-                    //    case Season.Summer2:
-                    //        {
-                    //            throw new NullReferenceException("Trying to start from a summer semester");
-                    //        }
-                    //}
-                }
-            }
-        }
+        //            //        }
+        //            //        break;
+        //            //    case Season.Summer1:
+        //            //        {
+        //            //            throw new NullReferenceException("Trying to start from a summer semester");
+        //            //        }
+        //            //    case Season.Summer2:
+        //            //        {
+        //            //            throw new NullReferenceException("Trying to start from a summer semester");
+        //            //        }
+        //            //}
+        //        }
+        //    }
+        //}
 
-        private void createTranscript(Season startSeason, int studentStartYear)
-        {
+        //private void createTranscript(Season startSeason, int studentStartYear)
+        //{
             
-        }
+        //}
 
-        public double GetSemesterGPA()
-        {
-            return 1.0;
-        }
+        //public double GetSemesterGPA()
+        //{
+        //    return 1.0;
+        //}
 
     }
 
