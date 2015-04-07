01f455d1153be482cfbcc2ef464e042e1e8cfc0a
marshbox <hilarychan079@gmail.com>
2015-04-07 00:27:58 -0400

    modified schedule css

--- Schedulator/Scripts/Script.js
+++ Schedulator/Scripts/Script.js
@@ -12,7 +12,9 @@ function courseAltColor(courseCode) {
     //var colors = ["#FAB586", "#DFDFDF", "#90C3F2", "#F9FE67", "#DFFFDB", "#E4BE5D", "#90DBC1", "#D9D0C9"];
     
     // red, orange, lightblue, lightgreen, purple,yellow,turquoise,grey
-    var colors = ["#e74c3c", "#f0ad4e", "#5bc0de", "#5cb85c", "#B284C5", "#f1c40f", "#1abc9c", "#bdc3c7"];
+    //var colors = ["#f06b5d", "#f0ad4e", "#5bc0de", "#5cb85c", "#B284C5", "#f1c40f", "#1abc9c", "#bdc3c7"];
+    //  grey, purple, lightblue, turquoise, lightgreen, yellow, orange,red
+    var colors = ["#bdc3c7", "#B284C5", "#5bc0de", "#1abc9c", "#5cb85c", "#f1c40f", "#f0ad4e", "#f06b5d"];
     
     if (colors[colorCount] == undefined || typeof colors[colorCount] == "undefined") {
         colorCount = 0;
