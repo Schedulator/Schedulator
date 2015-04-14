da86cac75a263a8db64d18ebbf750e3e83fab2dd
harley1011 <harley.1011@gmail.com>
2015-04-14 02:31:28 -0600

    Removed some scripts that weren't being used

--- Schedulator/Scripts/schedule.generator.js
+++ Schedulator/Scripts/schedule.generator.js
@@ -1,29 +1,4 @@
-﻿var colorCount = 0;
-
-/* Alternating color for each course in schedule
- * @param courseCode class for each diff block
- */
-function courseAltColor(courseCode) {
-    // var colors = ["#FAB586", "#DFDFDF", "#90C3F2", "#F9FE67", "#DFFFDB", "#E4BE5D", "#90DBC1", "#D9D0C9"];
-    var colors = ["#bdc3c7", "#B284C5", "#5bc0de", "#1abc9c", "#5cb85c", "#f1c40f", "#f0ad4e", "#f06b5d"];
-
-    if (colors[colorCount] == undefined || typeof colors[colorCount] == "undefined") {
-        colorCount = 0;
-    }
-
-    var shade = colors[colorCount];
-    colorCount += 1;
-
-    $("." + courseCode).css({
-        backgroundColor: shade,
-        color: "#FFFFFF"
-    });
-
-
-
-}
-
-function reloadCourseSequence()
+﻿function reloadCourseSequence()
 {
     $("#courseSequenceRecommend").load('/ScheduleGenerator/StudentsCourseSequence');
 }
@@ -44,8 +19,7 @@ $(document).ready(function () {
 
     $('#generateSch').click(function (e) {
         colorCount = 0;
-        //$("#prefOptions").slideUp('slow');
-
+        $("#prefOptions").slideUp('slow');
         if ($('#selectedCourses').is(':empty')) {
             e.preventDefault();
             $('#addCourseErr').delay(50).fadeOut().fadeIn('fast');
